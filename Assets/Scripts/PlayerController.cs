cfe7b64ad23dedd9b0e3e981d8ee953440cf695b
Raul Pavani <raul.pavani@hotmail.com>
2025-11-06 20:38:06 -0300

    fixing player animations and add jump anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -47,8 +47,14 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
         {
             rigidBody.AddForceY(jumpForce);
+            animator.SetTrigger("Jump");
             isGrounded = false;
         }
+    }
+
+    void LateUpdate()
+    {
+        animator.SetBool("isGrounded", isGrounded);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
